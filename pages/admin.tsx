16f8a3ea5c8503de3a713d83a000e9fa3be8c47b
jstiava <j.j.stiava@wustl.edu>
2025-02-15 16:05:54 -0600

    admin view

--- pages/admin.tsx
+++ pages/admin.tsx
@@ -1,3 +1,4 @@
+"use client"
 import { headerHeight } from "@/layout/AuthProvider";
 import { StripePrice, StripeProduct } from "@/types";
 import { OurFileRouter, useUploadThing } from "@/utils/uploadthing";
