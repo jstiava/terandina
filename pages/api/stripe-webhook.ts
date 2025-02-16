ed61f72ce5a93e7e9209c707fe883167f0c610e3
jstiava <j.j.stiava@wustl.edu>
2025-02-16 11:01:55 -0600

    webhook change for updating a price

--- pages/api/stripe-webhook.ts
+++ pages/api/stripe-webhook.ts
@@ -8,10 +8,9 @@ import { buffer } from 'micro';
 
 export const config = {
   api: {
-    bodyParser: false, // 🔥 Disable automatic JSON parsing
+    bodyParser: false
   },
 }
-
 
 export default async function handleRequest(
   req: NextApiRequest,
@@ -25,8 +24,6 @@ export default async function handleRequest(
 
     const sig = req.headers["stripe-signature"] as string;
     const buf = await buffer(req);
-    
-    // 🔥 Verify webhook signature
     const stripe = new Stripe(String(process.env.STRIPE_SECRET_KEY));
     const endpointSecret = process.env.STRIPE_WEBHOOK_SECRET as string;
     event = stripe.webhooks.constructEvent(
@@ -34,17 +31,16 @@ export default async function handleRequest(
       sig,
       endpointSecret
     );
-    
+
     res.status(200).json({ message: "Thank you, Stripe" })
   } catch (err: any) {
-    
     console.error("Webhook Error:", err.message);
     return res.status(400).send(`Webhook Error: ${err.message}`);
   }
-  
-  
+
+
   try {
-    
+
     const mongo = await Mongo.getInstance();
 
     if (event.type === "product.created") {
@@ -83,11 +79,15 @@ export default async function handleRequest(
 
     if (event.type === "price.created") {
       const price = event.data.object as Stripe.Price;
-      return await handlePriceCreatedEvent(price);    
+      return await handlePriceCreatedEvent(price);
     }
 
 
-    if (event.type === "price.updated") {
+    if (event.type === 'product.deleted') {
+      return;
+    }
+
+    if (event.type === "price.deleted" || event.type === 'price.updated') {
       const price = event.data.object as Stripe.Price;
 
       const theProduct = await mongo.clientPromise.db('products').collection('products').findOne({
@@ -98,27 +98,15 @@ export default async function handleRequest(
         return await handlePriceCreatedEvent(price)
       }
 
-      const thePrice = theProduct?.prices.find(p => p.id === price.id);
-
-      if (!thePrice) {
-        await mongo.clientPromise.db('products').collection('products').updateOne({
-          id: theProduct.id
-        }, {
-          prices: [...theProduct.prices, {
-            ...price,
-            product: theProduct.id,
-            unit_amount: 1
-          }]
-        })
-        return;
-      }
-
       const newPrices = theProduct.prices.filter(x => x.id != price.id);
-      newPrices.push({
-        ...price,
-        product: theProduct.id,
-        unit_amount: 1
-      });
+
+      if (event.type === 'price.updated') {
+        newPrices.push({
+          ...price,
+          product: theProduct.id,
+          unit_amount: 1
+        });
+      }
 
       await mongo.clientPromise.db('products').collection('products').updateOne({
         id: theProduct.id
@@ -126,14 +114,6 @@ export default async function handleRequest(
         prices: newPrices
       })
 
-      return;
-    }
-
-    if (event.type === 'product.deleted') {
-      return;
-    }
-
-    if (event.type === "price.deleted") {
       return;
     }
 
