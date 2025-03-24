d26d74fa4c9eb80d08939d08c8ee8af1dd956d6d
jstiava <j.j.stiava@wustl.edu>
2025-03-24 17:06:33 -0500

    formatting on values page

--- pages/our-values.tsx
+++ pages/our-values.tsx
@@ -20,12 +20,14 @@ export default function OurValuesPage() {
                 <div className="column relaxed center" style={{
                     maxWidth: '30rem',
                     width: "100%",
-                    padding: "4rem 0"
+                    padding: "4rem 2rem"
                 }}>
 
                     <Typography variant="h1">Our Values</Typography>
 
-                    <Typography variant="body1">
+                    <Typography variant="body1" sx={{
+                        lineHeight: '2rem'
+                    }}>
                         Welcome to Terandina! We are a 100% Native-owned business who specialize in authentic native wear that connects people to the beauty and history of Native artistry. Our products are handmade by skilled native artisans from the Andean region.
 
                         We offer an exquisite collection of ponchos, hoodies, and more, made with real alpaca and organic wool. Our promise to you is ultimate comfort and style made with sustainable and durable materials.
