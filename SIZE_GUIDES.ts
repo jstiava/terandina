d2d9f6f7ae30eb87c216fb5e506ad0e34ab2ec3b
jstiava <j.j.stiava@wustl.edu>
2025-04-07 18:33:48 -0500

    add size guide to blankets and ponchos

--- SIZE_GUIDES.ts
+++ SIZE_GUIDES.ts
@@ -16,6 +16,19 @@ const SIZE_GUIDES = {
         ["Waist", "26.00 in", "28.00 in", "32.00 in"],
         ["Shoulder Width", "26.00 in", "28.00 in", "32.00 in"],
         ["Sleeve Length", "26.00 in", "28.00 in", "32.00 in"],
+    ],
+    "blankets": [
+        ["Size", "Queen"],
+        ["Width", "26.00 in"],
+        ["Length", "26.00 in"],
+    ],
+    "ponchos": [
+        ["Size", "One-Size"],
+        ["Length", "26.00 in"],
+        ["Chest", "26.00 in"],
+        ["Waist", "26.00 in"],
+        ["Shoulder Width", "26.00 in"],
+        ["Sleeve Length", "26.00 in"],
     ]
 
 }
