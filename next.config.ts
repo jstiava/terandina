7dda5e076c16ab781cf1241b626b53082ec14096
jstiava <j.j.stiava@wustl.edu>
2025-04-21 13:23:23 -0500

    redirect to collections

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,15 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   reactStrictMode: true,
+  async redirects() {
+    return [
+      {
+        source: '/collections/:path*',
+        destination: '/products',
+        permanent: true,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
